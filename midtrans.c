e6fa826426bbe4fead87e63eabbc8b2284455bd8
ꦌꦫꦶꦏ꧀ꦦꦿꦧꦮꦑꦩꦭ꧀ <prabowo@darapsa.org>
2022-10-05 11:31:52 +0800

    CA info (for cURL) init & clean-up

--- midtrans.c
+++ midtrans.c
@@ -8,16 +8,26 @@
 #include <json.h>
 #include "midtrans.h"
 
+#ifndef __EMSCRIPTEN__
+static char *cainfo = NULL;
+#endif
+
 void midtrans_init(const char *certificate)
 {
 #ifndef __EMSCRIPTEN__
 	curl_global_init(CURL_GLOBAL_SSL);
+	if (certificate) {
+		cainfo = malloc(strlen(certificate) + 1);
+		strcpy(cainfo, certificate);
+	}
 #endif
 }
 
 void midtrans_cleanup()
 {
 #ifndef __EMSCRIPTEN__
+	if (cainfo)
+		free(cainfo);
 	curl_global_cleanup();
 #endif
 }
